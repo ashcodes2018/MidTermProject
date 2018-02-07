cb6116da115be39b97d5177759090718c9262a58
dhornback10 <you@example.com>
2018-02-07 16:56:17 -0500

    Got the jquery thing working finally

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
 $(document).ready(function() {
 
-	var totalIncome;
-	var totalIncomeBar;
-	var budgetBar;
-	var entertainmentBar;
-	var foodBar;
-	var clothingBar;
-	var billsBar;
-	var expenseType;
-	var totalExpenses;
+    var totalIncome;
+    var totalIncomeBar;
+    var budgetBar;
+    var entertainmentBar = $("#totalEnt").val();
+    var foodBar = $("#totalFood").val();
+    var clothingBar = $("#totalClothing").val();
+    var billsBar = $("#totalBills").val();
+    // var expenseType;
+    var totalExpenses;
+    var currentFoodValue = 0;
+    var expenseAmount;
 
-	$("#incomeForm").hide();
-	$("#expenseForm").hide();
+    $("#incomeForm").hide();
+    $("#expenseForm").hide();
 
-	$("#updateIncomeButton").click(function(event){
-		console.log("Hey");
-		$("#incomeForm").show();
-	});
-	$("#saveIncomeButton").click(function(event){
-		event.preventDefault();
-		totalIncome = $("#incomeInput").val();
-		$("#totalBudgetRoom").attr('max', totalIncome);
-		$("#incomeForm").hide();
-	});
-	$("#addExpenseButton").click(function(event){
-		$("#expenseForm").show();
-	});
-	$("#saveExpenseButton").click(function(event){
-		expenseType = $("#dropdownInput").val();
-		expenseAmount = $("#expenseAmount").val();
-		expenseDescription = $("#expenseDescription").val();
+    $("#updateIncomeButton").click(function(event) {
+        console.log("Hey");
+        $("#incomeForm").show();
+    });
+    $("#saveIncomeButton").click(function(event) {
+        event.preventDefault();
+        totalIncome = $("#incomeInput").val();
+        $("#totalBudgetRoom").attr('max', totalIncome);
+        $("#incomeForm").hide();
+    });
+    $("#addExpenseButton").click(function(event) {
+        $("#expenseForm").show();
+    });
+    $("#expenseForm").submit(function(event) {
+    	event.preventDefault();
+        var expenseType = $("#dropdownInput").val();
+        console.log(expenseType);
+        expenseAmount = $("#expenseAmount").val();
+                console.log(expenseAmount);
 
-		if (expenseType === "Food"){
-			$("#totalFood").
-		} else if (expenseType === "Clothing"){
-			
-		} else if (expenseType === "Entertainment"){
-			
-		} else if (expenseType === "Bills"){
-			
-		}
-		totalExpenses = 
-		$("#expenseForm").hide();
-	});
+        expenseDescription = $("#expenseDescription").val();
+
+        if (expenseType == "Food") {
+            function updateFoodProgress() {
+
+                foodBar += expenseAmount;
+            };
+            updateFoodProgress();
+
+        } else if (expenseType === "Clothing") {
+            function updateClothingProgress() {
+                clothingBar += expenseAmount;
+            };
+            updateClothingProgress();
+
+        } else if (expenseType === "Entertainment") {
+            function updateEntertainmentProgress() {
+                entertainmentBar += expenseAmount;
+            };
+            updateEntertainmentProgress();
+
+        } else if (expenseType === "Bills") {
+            function updateBillsProgress() {
+                billsBar += expenseAmount;
+            };
+            updateBillsProgress();
+        }
+        totalExpenses =
+            $("#expenseForm").hide();
+    });
+
+
+
+
+
+
+
+
 
 
 
